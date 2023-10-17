c4c36a89d87671dda60229ef5244843eda098ef4
2sammy <samuelmanoah61@gmail.com>
2023-10-17 21:48:13 +0300

    php tutorial

---
+++ _starter_files/01_output.php
@@ -0,0 +1,12 @@
+<?php
+  echo 123, 'hello sam';
+/* -------- Output & Comments ------- */
+
+/*
+  A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
+*/
+
+
+
+//  echo 'hello there';
+  
