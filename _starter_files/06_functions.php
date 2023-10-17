866229f155126a883bc7d33444d793b65542240e
2sammy <samuelmanoah61@gmail.com>
2023-10-17 23:28:37 +0300

    functions

--- _starter_files/06_functions.php
+++ _starter_files/06_functions.php
@@ -12,3 +12,32 @@
     // code to be executed
   }
 */
+function registerUser($email) {
+  echo   $email . ' register';
+}
+registerUser('sam');
+// email is argument then sam is a parameter
+
+
+function sum($n1, $n2) {
+  return $n1 + $n2;
+}
+
+ //echo sum(4,5);
+ $number = sum(4,5);
+ //echo $number;
+
+ function sumi($n1= 4, $n2= 5) {
+  return $n1 + $n2;
+ }
+ $number = sumi();
+ echo $number;
+
+ $sub = function ($n1, $n2) {
+  return $n1- $n2;
+ };
+ echo $sub(10, 5);
+ // arrow function
+ $multiply = fn($n2, $n3) => $n2 * $n3;
+ $boo = fn ($s2, $s3) => $s2 + $s3;
+
