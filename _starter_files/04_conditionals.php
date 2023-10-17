26bd4ce07e63e7643445af5c9a8cb4ff68791956
2sammy <samuelmanoah61@gmail.com>
2023-10-17 22:44:18 +0300

    conditionals

--- _starter_files/04_conditionals.php
+++ _starter_files/04_conditionals.php
@@ -33,3 +33,45 @@ if (condition) {
 
 
 /* -------- Switch Statements ------- */
+$age = 29;
+ if($age >= 20) {
+   echo'you are older to drink';
+ }
+
+ $t = 3;
+ if($t < 12) {
+  echo 'Good morning';
+ }
+ elseif($t <17) {
+  echo 'good afternoon';
+ }
+
+ else {
+  echo 'good evening';
+ }
+
+ $posts = [''];
+ if(!empty($posts) ) {
+  echo $posts[0];
+ } else {
+  echo "no posts";
+ }
+
+ // ternary
+ echo !empty($posts) ? $posts[0] : 'no posts';
+
+ // switches
+ $favcolor = 'red';
+ switch($favcolor) {
+  case 'red';
+    echo 'my fav';
+    break;
+    case 'blue';
+    echo 'my fav is blue';
+    break;
+    case 'black';
+    echo 'fav is black';
+    break;
+    default:
+    echo 'you dont have fav';
+ }
