005d980329e15f06a2a8d1250d5aea612d149a3d
2sammy <samuelmanoah61@gmail.com>
2023-10-18 08:05:52 +0300

    array_functions

--- _starter_files/08_string_functions.php
+++ _starter_files/08_string_functions.php
@@ -5,3 +5,8 @@
   Functions to work with strings
   https://www.php.net/manual/en/ref.strings.php
 */
+$string2 = '<script>alert(1)</script>';
+echo htmlspecialchars($string2);
+
+printf('%s likes to %s', 'Brad', 'code');
+printf('1+1 =%d', 1+1);
