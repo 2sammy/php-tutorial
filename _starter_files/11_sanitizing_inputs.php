c96b364b681a5dab1b5bb687ef90872b99f2028a
2sammy <samuelmanoah61@gmail.com>
2023-10-18 17:44:43 +0300

    sanitizing

--- _starter_files/11_sanitizing_inputs.php
+++ _starter_files/11_sanitizing_inputs.php
@@ -4,3 +4,38 @@
 /*
   Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
 */
+?>
+a<?php
+/* --- $_GET & $_POST Superglobals -- */
+
+/*
+  We can pass data through urls and forms using the $_GET and $_POST superglobals.
+*/
+
+if(isset($_POST['submit'])){
+  $name= htmlspecialchars( $_POST['name']);
+echo $_POST['age'];
+
+}
+//echo $_GET['name'];
+//echo $_POST['age'];
+?>
+
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>"
+method="$_POST">
+
+  <div>
+    <label form="name"> Name: </label>
+    <input type="text" name="name">
+  </div>
+
+
+  <div>
+    <label form="age"> Age: </label>
+    <input type="text" name="age">
+  </div>
+
+<input type="submit" value="Submit" name="submit">
+
+</form>
